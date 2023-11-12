3f81b79ce8ff80b59f17da118bd6ab73d8f278b9
Taianne Lima <rtaianne@gmail.com>
2023-11-11 19:57:04 -0800

    Taianne program

---
+++ taianneConditionals.js
@@ -0,0 +1,19 @@
+
+var numberTeachers = 4
+var numberStudents = 20
+var stringTeachers = "4"
+var numberDogs = 0
+
+// EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
+console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+// this should log: "Is numberTeachers greater than numberStudents?" false
+
+// YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+// this should log: false
+
+// YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+// this should log: true
+
+// YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+// this should log: false
+
