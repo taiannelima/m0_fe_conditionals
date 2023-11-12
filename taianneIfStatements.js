79e6ae695e88f70448e4ddf518faca09d3a5c42e
Taianne Lima <rtaianne@gmail.com>
2023-11-11 20:40:25 -0800

    IfStatements Done

---
+++ taianneIfStatements.js
@@ -0,0 +1,48 @@
+// In the below exercises, write code that achieves the desired result. 
+// To check your work, run this file by entering the following command in your Terminal: `node ifStatements.js`
+
+// CHALLENGE 1
+// Using the numQuarters variable defined below, determine if you have enough money to buy a gumball. A gumball costs two quarters.
+
+// Right now, the program will print out both "I have enough money for a gumball" and "I don't have enough money for a gumball". 
+// Write a conditional statement that prints only one or the other.
+// Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
+
+var numQuarters = 0;
+if (numQuarters >=2) {
+
+console.log("I have enough money for a gumball");
+} else {
+
+console.log("I don't have enough money for a gumball");
+}
+
+
+// CHALLENGE 2
+// Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
+// A pizza requires at least two cups of flour and sauce.
+
+// You should be able to change the variables to achieve the following outputs:
+// If cupsOfFlour = 1 and hasSauce = true, print "I cannot make pizza"
+// If cupsOfFlour = 5 and hasSauce = false, print "I cannot make pizza"
+// If cupsOfFlour = 2 and hasSauce = true, print "I can make pizza"
+// If cupsOfFlour = 3 and hasSauce = true, print "I can make pizza"
+
+// Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
+
+var cupsOfFlour = 3;
+var hasSauce = true;
+if (cupsOfFlour === 1 && hasSauce === true){
+    console.log ("I cannot make pizza")
+}
+
+if (cupsOfFlour ===5 && hasSauce === false){
+    console.log ("I cannot make Pizza")
+}
+if (cupsOfFlour === 2 && hasSauce === true){
+    console.log ("I can make pizza")
+}
+
+if (cupsOfFlour === 3 && hasSauce === true){
+    console.log ("I can make pizza")
+}
